efa5576eefa7a7879db68fea1abc1c2136eda766
nguvictor <victor.nguyen@hotmail.com>
2014-03-07 12:29:50 -0500

    HTML5 WIP

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link type="text/css" rel="stylesheet" media="all" href="assets/css/jquery.mmenu.css" />
+<link type="text/css" rel="stylesheet" href="assets/css/main.css" />
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+   
+   <script type="text/javascript" src="assets/js/jquery.mmenu.js"></script>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCpCZg9odVngPubFm4-F22a1k2lVzpRSOY&sensor=true">
+    </script>
+   <script src="assets/js/main.js"></script>
+   <style type="text/css">
+		   html { height: 100%;width: 100%; }
+		   
+		  body { height: 100%;width: 100%; margin: 0; padding: 0 }
+			a:hover, a:visited, a:link, a:active
+			{
+				text-decoration: none;
+			}
+			#page{ height: 100% }
+			#content { height: 100%;padding:0px; }
+			 #map-canvas { height: 100% }
+	</style>
+</head>
+
+<body>
+   <div id="page">
+			<div id="header">
+				<a href="#menu"></a>
+				Wander
+			</div>
+			<div id="content">
+				<div id="map-canvas"/>
+			</div>
+			<nav id="menu">
+				<ul>
+					<li><a href="#">Tours</a></li>
+					<li><a href="#">My Locations</a></li>
+					<li><a href="#">Points Of Interest</a></li>
+				</ul>
+			</nav>
+		</div>
+</body>
+</html>
