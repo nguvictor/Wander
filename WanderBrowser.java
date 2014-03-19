4eb07783274b44f5d53488007b99c0a9ee0cf34e
ShawnQuach <shawn.quach@ryerson.ca>
2014-03-19 01:57:48 -0400

    Create WanderBrowser.java

---
+++ WanderBrowser.java
@@ -0,0 +1,18 @@
+package com.example.browser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class SimpleBrowser extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_simple_browser);
+		
+		WebView myWebView = (WebView) findViewById(R.id.webview);
+		myWebView.loadUrl("http://wander.site90.com/");
+	}
+
+}
