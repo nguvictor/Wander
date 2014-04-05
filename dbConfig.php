efe436bb2914fcc012344629b984b5435c7e4931
nguvictor <victor.nguyen@hotmail.com>
2014-04-05 13:55:23 -0400

    Added wander.site90.com mysql credential to dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -3,6 +3,14 @@ $location = "localhost";
 $user = "root";
 $pass = "";
 $db = "wander";
+
+//wander.site90.com credenticals
+/*
+$location = "mysql4.000webhost.com";
+$db = "a2839918_wander";
+$user = "a2839918_wander";
+$pass = "wander123";
+ * */
 /*
 $con=mysqli_connect($location,$user,$pass,$db);
 
